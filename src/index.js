22d7359b0bdba1049ae38e2745d8ec57eca087ef
Joe Chandler <josephalanchandlerjr@gmail.com>
2019-12-18 10:56:37 -0500

    create initial index.js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('on line');
+const express 		= require('express'),
+	  app			= express(),
+	  mongoose		= require('mongoose'),
+	  userRouter 	= require('./routers/user'),
+	  bugRouter		= require('./routers/bug'),
+	  port			= process.env.PORT || 3000;
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/bug-tracker', {
+	useNewUrlParser: true, 
+	useCreateIndex: true,
+	useUnifiedTopology: true
+});
+
+//comfigure app
+app.use(express.json()); //parse body as json and provide via req.body
+app.use(userRouter);
+app.use(bugRouter);
+
+
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));	
